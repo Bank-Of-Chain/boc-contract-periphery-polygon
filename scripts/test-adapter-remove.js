89588724f9aedd1db3392a5db8329e7a3fa9b0ae
韩旭 <js_bin@163.com>
2022-08-04 16:17:27 +0800

    refactor: add remarks and code formate

--- scripts/test-adapter-remove.js
+++ scripts/test-adapter-remove.js
@@ -4,34 +4,30 @@ const ExchangeAggregator = hre.artifacts.require("ExchangeAggregator");
 // === Utils === //
 const { findIndex } = require("lodash");
 
+// Set the vault_address in chain-137 which should remove testAdapter
 const VAULT_ADDRESS = "0x8a8fA8b26B8D2743ECd3c687cCDB345061D70540";
 
 const main = async () => {
-  const vault = await Vault.at(VAULT_ADDRESS);
+    const vault = await Vault.at(VAULT_ADDRESS);
 
-  const exchangeAggregatorAddress = await vault.exchangeManager();
-  const exchangeAggregator = await ExchangeAggregator.at(
-    exchangeAggregatorAddress
-  );
-  const {
-    "0": adapterArray,
-    "1": adapterIdentifier
-  } = await exchangeAggregator.getExchangeAdapters();
+    const exchangeAggregatorAddress = await vault.exchangeManager();
+    const exchangeAggregator = await ExchangeAggregator.at(exchangeAggregatorAddress);
+    const { "0": adapterArray, "1": adapterIdentifier } = await exchangeAggregator.getExchangeAdapters();
 
-  const index = findIndex(adapterIdentifier, i => i === "testAdapter");
+    const index = findIndex(adapterIdentifier, i => i === "testAdapter");
 
-  if (index === -1) {
-    console.log("has no testAdapter in ExchangeAdapters array");
-    return;
-  }
-  // remove testAdapter
-  await exchangeAggregator.removeExchangeAdapters([adapterArray[index]]);
-  console.log(await exchangeAggregator.getExchangeAdapters());
+    if (index === -1) {
+        console.log("has no testAdapter in ExchangeAdapters array");
+        return;
+    }
+    // remove testAdapter
+    await exchangeAggregator.removeExchangeAdapters([adapterArray[index]]);
+    console.log(await exchangeAggregator.getExchangeAdapters());
 };
 
 main()
-  .then(() => process.exit(0))
-  .catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
